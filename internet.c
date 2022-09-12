8c6d497c3afe642c730487e028767db011c645c9
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2022-09-12 14:24:37 +0530

    Update internet.c

--- internet.c
+++ internet.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node{
+    int status; //working or crashed
+    short int packet;
+    int IP;
+    struct node* adjacent;
+} *home;
+
+void data2packets(FILE * f){
+  
+}
+
+int main(){
+    printf("You are now connected to your home wifi!!\v\n");
+    struct node* myPC = home;
+
+    while(home->status != 0 ){
+
+    }
+    printf("ERROR: No signal!\n");
+    return 0;
+}
