7d59d224e37342b846b7e4e0b01fef7329a5f70a
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2022-09-13 23:39:15 +0530

    Update internet.c

--- internet.c
+++ internet.c
@@ -1,46 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-    int status; // working(1) or crashed(0)
+// for simplicity, assume IP addresses are characters
+
+
+struct router{
+    char IP;
+    int status;     // working(1) or not(0)
     int packet;
-    int IP;
-    struct node* adjacent;
-} home;
+    int trigger;    // packet being processed(1) or not(0)   
+    struct router* adjacent;
+} *initial_router;
 
+struct connection{  // database or user
+    char IP;
+    FILE* A[10];    // data storing unit
+    struct router* connected_router;
+}myPC,poem,math;
 
-void data2packets( char ){
+void internet(){
+    ;
+}
+
+int data2packets(char c){
+    ;
+}
+
+void route(int packets){
+    ;
+}
+
+void interact(char IP){
     ;
 }
 
 int main(){
+
+// setting simple IPs to avoid use of DNS
+    poem.IP = 'a';
+    math.IP = 'b';
+    myPC.IP = 'c';
+
+// starting the internet
+    internet();
+
+// connected to a router
 beginning:
     printf("\n\tYou are now connected to a wifi.\n");
-    struct node *myPC = &home;
-    myPC->status = 1;
-    char c;    // choice
+    myPC.connected_router = initial_router;
+    myPC.connected_router -> status = 1;
+    char choice;
 
-    while(myPC->status == 1 ){
-        printf("\na) Read a poem\nb) Solve maths problms\nc) Crash the router!!\nWhat would you like to do?: ");
-        scanf("%c",&c);
+    // some internet surfing
+    while(myPC.connected_router -> status == 1 ){
+        printf("\na) Read a poem\nb) Solve maths problms\nc) Crash the router!!\nWhat would yo like to do?: ");
+        scanf("%c",&choice);
         getchar();
-        if(c != 'c') {
-            data2packets(c);
-        }
-        else {
-            myPC->status = 0;
-        }
+        if(choice != 'c') interact(choice);
+        else myPC.connected_router -> status = 0;
     }
-    printf("\n\tERROR: No signal!\n\tWould you like to connect to your another wifi?(y/n): ");
-    scanf("%c",&c);
+
+// No signal
+    printf("\n\tERROR: No signal!\n\tWould you like to connect to another wifi?(y/n): ");
+    scanf("%c",&choice);
     getchar();
-    if ( c == 'y'){
-        myPC = myPC->adjacent;
-        myPC->status = 1;
+    if ( choice == 'y'){
+        myPC.connected_router = myPC.connected_router -> adjacent;
+        myPC.connected_router -> status = 1;
         goto beginning;
     }
     else {
         printf("\n\tPower off\n");
     }
+
     return 0;
 }
